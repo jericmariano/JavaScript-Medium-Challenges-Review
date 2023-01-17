bb21eb025d5179b7363d455586274c0ae2f9ae22
Jeric <marianovjeric@gmail.com>
2023-01-16 23:09:03 -0800

    convert to zeros function

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-function reverseString(str) {
-    return str.split('').reverse().join('')
+function convertToZeros(arr) {
+
+
+    // for loop method 1
+    // for (let i = 0; i < arr.length; i++) {
+    //     arr[i] = 0
+    // }
+    // return arr
+
+    // ------------------- //
+
+    // array . fill method 2
+    // return arr.fill(0)
+
+    // ------------------- //
+
+    // array . map method 3
+    return arr.map(elem => 0)
+
 }
 
-console.log(reverseString('jeric'))
+console.log(convertToZeros([5, 100, 0]))
