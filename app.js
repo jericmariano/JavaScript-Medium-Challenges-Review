7912728168416490661d6d85b742d0b0d3a97dc4
Jeric <marianovjeric@gmail.com>
2023-01-16 23:20:50 -0800

    filter out falsy values

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-function removeApples(arr) {
-    return arr.filter(elem => elem !== 'Apple')
+function filterOutFalsy(arr) {
+    let newArr = []
+
+    for(let i = 0; i < arr.length; i++) {
+        if (!!arr[i] === true) {
+            newArr.push(arr[i])
+        }
+    }
+    return newArr
 }
 
-console.log(removeApples(['Banana', 'Apple', 'Orange', 'Apple']))
+console.log(filterOutFalsy(['Tomato', 'Orange', 'Banana', false]))
