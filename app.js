03d96a0b5a1d3a017d1de26dab4434d93d5a7b24
Jeric <marianovjeric@gmail.com>
2023-01-16 23:21:31 -0800

    filter out falsy values with arr.filter

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 function filterOutFalsy(arr) {
-    let newArr = []
-
-    for(let i = 0; i < arr.length; i++) {
-        if (!!arr[i] === true) {
-            newArr.push(arr[i])
-        }
-    }
-    return newArr
+    return arr.filter(elem => !!elem === true)
 }
 
 console.log(filterOutFalsy(['Tomato', 'Orange', 'Banana', false]))
