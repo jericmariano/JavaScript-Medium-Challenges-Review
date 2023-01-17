87b5b415fa6e580814717f8a77ae06ceb8fddfe1
Jeric <marianovjeric@gmail.com>
2023-01-16 23:14:39 -0800

    filter out apples function

--- app.js
+++ app.js
@@ -1,22 +1,5 @@
-function convertToZeros(arr) {
-
-
-    // for loop method 1
-    // for (let i = 0; i < arr.length; i++) {
-    //     arr[i] = 0
-    // }
-    // return arr
-
-    // ------------------- //
-
-    // array . fill method 2
-    // return arr.fill(0)
-
-    // ------------------- //
-
-    // array . map method 3
-    return arr.map(elem => 0)
-
+function removeApples(arr) {
+    return arr.filter(elem => elem !== 'Apple')
 }
 
-console.log(convertToZeros([5, 100, 0]))
+console.log(removeApples(['Banana', 'Apple', 'Orange', 'Apple']))
